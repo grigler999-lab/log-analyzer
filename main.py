f819563115ac8291200feadc1b364c1c0d4207bb
Hlib Hrynchenko <grigler999@gmail.com>
2026-04-15 14:21:23 +0200

    feat: display warning log entries

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def analyze_logs(lines):
     warning_count = 0
     error_count = 0
     error_lines = []
+    warning_lines = []
 
     for line in lines:
         line = line.strip()
@@ -27,11 +28,12 @@ def analyze_logs(lines):
             info_count += 1
         elif "WARNING" in line:
             warning_count += 1
+            warning_lines.append(line)
         elif "ERROR" in line:
             error_count += 1
             error_lines.append(line)
 
-    return info_count, warning_count, error_count, error_lines
+    return info_count, warning_count, error_count, warning_lines, error_lines
 
 
 def show_summary(info_count, warning_count, error_count, ):
@@ -50,6 +52,16 @@ def show_errors(error_lines):
     for line in error_lines:
         print(line)
 
+def show_warnings(warning_lines):
+    print("\nWarning lines:")
+
+    if not warning_lines:
+        print("No warnings found.")
+        return
+
+    for line in warning_lines:
+        print(line)
+
 
 def main():
     filename = "sample.log"
@@ -65,9 +77,10 @@ def main():
         return
     
     
-    info_count, warning_count, error_count, error_lines = analyze_logs(lines)
+    info_count, warning_count, error_count, warning_lines, error_lines = analyze_logs(lines)
 
     show_summary(info_count, warning_count, error_count, )
+    show_warnings(warning_lines)
     show_errors(error_lines)
 
 
